ea4d22287197465e599f7a965dbf9634255393eb
Maarja Kukk <62176348+maarja-k@users.noreply.github.com>
2026-06-02 12:38:34 +0300

    Juurde tunnus, mis vaatleb rohkemate toitainete andmeid

--- dbt_project/models/marts/mart_data_completeness.sql
+++ dbt_project/models/marts/mart_data_completeness.sql
@@ -2,6 +2,7 @@ select
     count(*) as total_products,
     avg(completeness_score) as avg_completeness_score,
     sum(has_nutrition_info::int) as products_with_nutrition,
+    sum(has_eu_nutrition_info::int) as products_with_eu_nutrition,
     sum(has_ingredients::int) as products_with_ingredients,
     sum(has_packaging::int) as products_with_packaging,
     sum(has_quantity::int) as products_with_quantity
